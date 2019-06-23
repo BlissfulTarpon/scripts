c666a6fa4fe41a3627fefe0074ccee6a6d02d958
fensiv <50057402+fensiv@users.noreply.github.com>
2019-06-23 16:39:15 -0400

    traduit fr

--- ipsweep.sh
+++ ipsweep.sh
@@ -3,7 +3,7 @@
 
 if [ "$1" == "" ]
     then
-        echo "You need to provide the network"
+        echo "Vous devez fournir votre subnet"
         echo "Syntax: ./ipsweep.sh 192.168.1"
     else
         for ip in `seq 1 254`; do
