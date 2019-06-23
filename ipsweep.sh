3f9ecfe652580ce637b7f79e8ed8c7603b2b452b
fensiv <50057402+fensiv@users.noreply.github.com>
2019-06-23 16:37:52 -0400

    Add files via upload
    Initial up

---
+++ ipsweep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script taken from @thecybermentor and @GeorgiaWeidman
+
+if [ "$1" == "" ]
+    then
+        echo "You need to provide the network"
+        echo "Syntax: ./ipsweep.sh 192.168.1"
+    else
+        for ip in `seq 1 254`; do
+        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
